0a884993c322c24cbe2183119f79720a2fe6c521
Amirul <amirul.naim@zuscoffee.com>
2023-11-06 14:09:39 +0800

    Configure deploy github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   devtools: { enabled: true }
 })
