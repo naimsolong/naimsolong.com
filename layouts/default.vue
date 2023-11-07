edf97c838e7c2a8df7bd6f2cfddcd0e82bee42ad
Amirul <amirul.naim@zuscoffee.com>
2023-11-07 20:48:45 +0800

    Update body class

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 useHead({
     bodyAttrs: {
         class: computed(() => {
-            return 'max-w-screen-md px-10 md:px-0 mx-auto';
+            return 'max-w-screen-lg px-10 md:px-0 mx-auto';
         }),
     },
 });
