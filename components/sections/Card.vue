9945c137de58d6e306a81db4affe3a66d50b7b15
Amirul <amirul.naim@zuscoffee.com>
2023-12-15 16:17:31 +0800

    Update card

--- components/sections/Card.vue
+++ components/sections/Card.vue
@@ -14,9 +14,11 @@ const props = defineProps({
         <a :href="modifyLink('posts', props.post.slug)">
             <img v-if="props.post.image && props.post.image != ''" class="rounded-t-lg" :src="props.post.image" :alt="props.post.alt ?? 'Images'" />
             <div class="flex flex-col justify-between p-5 h-full">
-                <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">{{ props.post.headline }}</h5>
-                <p class="mb-3 font-normal text-gray-700">{{ props.post.description }}</p>
-                <span class="inline-flex mt-3 items-center text-sm font-medium text-center rounded-lg">
+                <span>
+                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">{{ props.post.headline }}</h5>
+                    <p class="mb-3 font-normal text-gray-700">{{ props.post.description }}</p>
+                </span>
+                <span class="inline-flex pt-3 items-center text-sm font-medium text-center border-t-2 border-ray-200">
                     Read more
                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
