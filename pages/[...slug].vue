494a8ad07e5ead001799bce6f31463510fc243f8
Amirul <amirul.naim@zuscoffee.com>
2024-02-09 11:56:43 +0800

    Simple page not found

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,12 @@
 <template>
   <main>
-    <ContentDoc />
+      <ContentDoc :path="$route.path">
+        <template v-slot="{ doc }">
+          <ContentRenderer :value="doc" />
+        </template>
+        <template #not-found>
+          <h1>Page not found</h1>
+        </template>
+      </ContentDoc>
   </main>
 </template>
