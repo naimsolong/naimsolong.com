d956bbed928774f6de3f47aa031ff693cce92348
Amirul <naimsolong@gmail.com>
2024-04-19 09:07:53 +0800

    Update landing page

--- components/content/SectionTech.vue
+++ components/content/SectionTech.vue
@@ -8,6 +8,22 @@ import { stacks } from '~/datas/root'
             By using latest technology stack and tools that can scale but not limited to.
         </FontsParagraph>
 
-      <SectionsImages :datas="stacks"/>
+        <FontsHeaderTwo text_align="center">
+            Back End Stack
+        </FontsHeaderTwo>
+
+        <SectionsImages :datas="stacks.backend"/>
+
+        <FontsHeaderTwo text_align="center">
+            Front End Stack
+        </FontsHeaderTwo>
+
+        <SectionsImages :datas="stacks.frontend"/>
+
+        <FontsHeaderTwo text_align="center">
+            DevOps Stack
+        </FontsHeaderTwo>
+
+        <SectionsImages :datas="stacks.devops"/>
     </SectionsSpacing>
 </template>
--- components/fonts/HeaderTwo.vue
+++ components/fonts/HeaderTwo.vue
@@ -1,5 +1,35 @@
+<script setup lang="ts">
+const props = defineProps({
+    text_align: {
+        type: String,
+        default: 'justify',
+    },
+})
+
+const classes = computed(() => {
+    let alignment = 'text-justify'
+
+    if(props.text_align === 'center') {
+        alignment = 'text-center'
+    } else if(props.text_align === 'right') {
+        alignment = 'text-right'
+    } else if(props.text_align === 'left') {
+        alignment = 'text-left'
+    }
+
+    return [
+        'text-2xl',
+        'font-extrabold',
+        'mt-10',
+        'mb-5',
+        'tracking-wider',
+        alignment,
+    ]
+})
+</script>
+
 <template>
-    <h2 class="text-2xl font-extrabold mt-10 mb-5 tracking-wider">
+    <h2 :class="classes">
         <slot /> 
     </h2>
 </template>
