fa4632c4b663f673975d9d69b2402ff55238b005
Amirul <naimsolong@gmail.com>
2024-05-16 06:51:35 +0800

    Restructure head

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,26 @@ useHead({
             rel: 'canonical',
             href: config.public.app_url+route.path,
         },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com',
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+        },
     ],
+    script: [
+        {
+            defer: true,
+            src: 'https://static.cloudflareinsights.com/beacon.min.js',
+            "data-cd-beacon": '{"token": "'+cloudflare_token+'"}'
+        }
+    ]
 });
 </script>
 
@@ -26,14 +45,6 @@ useHead({
 </style>
 
 <template>
-    <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-
-        <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "{{ token }}"}'></script><!-- End Cloudflare Web Analytics -->
-    </Head>
-
     <main>
         <HeadersDefault />
         <SectionsSpacing>
